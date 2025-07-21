046721f63d6ec63f63813a5085901d5d4afb6d60
IchChefDuNixx <minecraftkuchen@gmail.com>
2025-07-21 17:17:12 +0200

    v1.0.1 patch

--- Assets/Scripts/EnableAbilities.cs
+++ Assets/Scripts/EnableAbilities.cs
@@ -2,11 +2,22 @@ using UnityEngine;
 
 public class EnableAbilities : MonoBehaviour {
 
-    private void OnTriggerEnter2D(Collider2D other) {
-        if (other.CompareTag("Felix") || other.CompareTag("Anna")) {
+    private bool activated;
+    private Room room;
+
+    public void Start() {
+        activated = false;
+        room = GetComponent<Room>();
+    }
+
+    private void OnTriggerStay2D(Collider2D other) {
+        if (!other.CompareTag("Felix") && !other.CompareTag("Anna")) return;
+
+        if (!activated && LevelManager.instance.currentRoom == room) {
             LevelManager.instance.UnlockAbility(Shrine.Ability.Dash);
             LevelManager.instance.UnlockAbility(Shrine.Ability.SpawningPlatforms);
             LevelManager.instance.UnlockAbility(Shrine.Ability.SwitchingPlatforms);
+            activated = true;
         }
     }
 }
